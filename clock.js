e490270933dc6a69f25f5a7f9608ced6365b8984
ISHIIAoi00 <73536128+ISHIIAoi00@users.noreply.github.com>
2020-11-28 19:03:07 +0900

    initial commit

---
+++ clock.js
@@ -0,0 +1,28 @@
+function clock() {
+
+  let twoDigit = function(num) {
+    let digit;
+    if (num < 10) {
+      digit = '0' + num;
+    } else {
+      digit = num;
+    }
+    return digit;
+  }
+  let weeks = new Array('SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT');
+
+  let time = new Date();
+  
+  let year = time.getFullYear();
+  let month = twoDigit(time.getMonth() + 1);
+  let day = twoDigit(time.getDay());
+  let week = weeks[time.getDay()];
+  let hour = twoDigit(time.getHours());
+  let min = twoDigit(time.getMinutes());
+  let sec = twoDigit(time.getSeconds());
+
+  document.getElementById('week').textContent = week;
+  document.getElementById('date').textContent = year + '/' + month + '/' + day;
+  document.getElementById('time').textContent = hour + ':' + min + ':' + sec;
+}
+setInterval(clock, 1000);
